c289248354d18d2160667a4a190007871894597a
Marten Veldthuis <marten@veldthuis.com>
2017-01-16 17:53:51 +0000

    Add FakePanoptes

--- app/models/effects.rb
+++ app/models/effects.rb
@@ -1,6 +1,21 @@
 module Effects
+  class FakePanoptes
+    def method_missing(method_name, *args)
+      Rails.logger.info(">>> Panoptes API call [#{method_name}], args: #{args.inspect}")
+      nil
+    end
+  end
+
   def self.panoptes
-    @panoptes
+    return @panoptes if @panoptes
+
+    if ENV.key?("PANOPTES_CLIENT_ID") || Rails.env.staging? || Rails.env.production?
+      @panoptes = Panoptes::Client.new(env: Rails.env.to_s,
+                                       auth: {client_id: ENV.fetch("PANOPTES_CLIENT_ID"),
+                                              client_secret: ENV.fetch("PANOPTES_CLIENT_SECRET")})
+    else
+      @panoptes = FakePanoptes.new
+    end
   end
 
   def self.panoptes=(adapter)
--- config/initializers/panoptes.rb
+++
@@ -1,3 +0,0 @@
-Effects.panoptes = Panoptes::Client.new(env: Rails.env.to_s,
-                                        auth: {client_id: ENV.fetch("PANOPTES_CLIENT_ID"),
-                                               client_secret: ENV.fetch("PANOPTES_CLIENT_SECRET")})
