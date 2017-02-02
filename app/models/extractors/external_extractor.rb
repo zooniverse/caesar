129d18fb2245e076e8c648c00183c7159a2d9df4
Marten Veldthuis <marten@veldthuis.com>
2017-02-02 15:51:27 +0000

    add process method to external extractor

--- app/models/extractors/external_extractor.rb
+++ app/models/extractors/external_extractor.rb
@@ -1,4 +1,13 @@
 module Extractors
   class ExternalExtractor < Extractor
+    def process(classification)
+      return {}
+
+      # TODO: If URL configured, send classification there
+    end
+
+    def url
+      config.fetch
+    end
   end
 end
