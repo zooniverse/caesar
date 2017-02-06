1e77915261c5f3f4c74aba7e2d21714284f61bb3
Amy Rebecca <amyrebecca@users.noreply.github.com>
2017-02-06 16:16:16 +0000

    Create ExternalReducer (#17)
    * new effect to add a given subject to a configured subject set, closes #12
    
    * Add an external reducer / HTTP reducer (closes #10)

--- app/models/reducers/external_reducer.rb
+++ app/models/reducers/external_reducer.rb
@@ -1,4 +1,24 @@
+require 'uri'
+
 module Reducers
   class ExternalReducer < Reducer
+    def process(extractions)
+      if url
+        req = Net::HTTP::Post.new(url, 'Content-Type' => 'application/json')
+        req.body = extractions.to_json
+        res = Net::HTTP.start(url.hostname, url.port) do |http|
+          http.request(req)
+        end
+        JSON.parse(res.body)
+      else
+        {}
+      end
+    end
+
+    def url
+      return nil unless config.key?('url')
+
+      @url ||= URI(config['url'])
+    end
   end
 end
---
+++ spec/models/reducers/external_reducer_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Reducers::ExternalReducer do
+  let(:extracts) { [
+    {"foo" => "bar"} ,
+    {"foo" => "baz"}
+  ] }
+
+  let(:response_data) { {"result" => {"bar" => 1, "baz" => 1}} }
+
+  before do
+    stub_request(:post, "http://example.org/post/extracts/here").
+      with(:body => extracts.to_json,
+           :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'Host'=>'example.org', 'User-Agent'=>'Ruby'}).
+      to_return(:status => 200, :body => response_data.to_json, :headers => {})
+  end
+
+  it 'posts the extracts to a foreign API' do
+    reducer = described_class.new("red", "url" => "http://example.org/post/extracts/here")
+    reducer.process(extracts)
+
+    expect(a_request(:post, "example.org/post/extracts/here")
+            .with(body: extracts.to_json))
+      .to have_been_made.once
+  end
+
+  it 'passes through the result from the foreign API' do
+    extractor = described_class.new("red", "url" => "http://example.org/post/extracts/here")
+    result = extractor.process(extracts)
+    expect(result).to eq(response_data)
+  end
+
+  it 'does not post if no url is configured' do
+    reducer = described_class.new("red")
+    reducer.process(extracts)
+
+    expect(a_request(:post, "example.org/post/extracts/here")
+            .with(body: extracts.to_json))
+      .not_to have_been_made
+  end
+
+end
