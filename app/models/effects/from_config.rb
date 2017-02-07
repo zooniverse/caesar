c4d7ce53da10f9278e999b9d2c2993edf53673fe
Marten Veldthuis <marten@veldthuis.com>
2017-02-07 14:37:38 +0000

    Allow new effects to be read from config

--- app/models/effects/from_config.rb
+++ app/models/effects/from_config.rb
@@ -6,6 +6,10 @@ module Effects
       case config["action"].to_s
       when "retire_subject"
         RetireSubject.new(config)
+      when "add_subject_to_set"
+        AddSubjectToSet.new(config)
+      when "add_subject_to_collection"
+        AddSubjectToCollection.new(config)
       else
         raise UnknownEffect, "Don't know what to do with #{config.inspect}"
       end
