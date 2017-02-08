a792931a7914f1a053a40772fe82e99eb4cdd5e5
Amy Boyer <amy@zooniverse.org>
2017-02-08 16:49:22 +0000

    actually do uniquing that will work for extract model objects

--- app/models/reducers/unique_count_reducer.rb
+++ app/models/reducers/unique_count_reducer.rb
@@ -1,7 +1,17 @@
 module Reducers
   class UniqueCountReducer < Reducer
     def process(extracts)
-      extracts.uniq.size
+      mapped = extracts.map do |extract|
+        if extract.data.key?(unique_field)
+          extract.data[unique_field]
+        end
+      end
+
+      mapped.uniq.size
+    end
+
+    def unique_field
+      config["field"]
     end
   end
 end
--- spec/models/reducers/unique_count_reducer_spec.rb
+++ spec/models/reducers/unique_count_reducer_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 
 describe Reducers::UniqueCountReducer do
-  let(:reducer){ described_class.new("s",{})}
-
-  it 'counts things' do
-    expect(reducer.process([])).to eq(0)
-    expect(reducer.process([3, 4, 5, 6])).to eq(4)
-    expect(reducer.process([{"user_id"=>3},{"user_id"=>4}])).to eq(2)
-  end
+  let(:reducer){ described_class.new("s",{"field" => "choices"})}
+  let(:extracts){
+    [
+      Extract.new(
+        :classification_id => 1234,
+        :classification_at => Date.new(2017,2,5),
+        :data => { "choices" => ["NTHNGHR"] }
+      ),
+      Extract.new(
+        :classification_id => 1235,
+        :classification_at => Date.new(2017,2,5),
+        :data => { "choices" => ["RCCN", "RCCN"]}
+      ),
+      Extract.new(
+        :classification_id => 1237,
+        :classification_at => Date.new(2017,2,7),
+        :data => { "choices" => ["NTHNGHR"] }
+      )
+    ]
+  }
 
   it 'counts unique things' do
-    expect(reducer.process([3,4,3,6])).to eq(3)
-    expect(reducer.process([{"user_id"=>3},{"user_id"=>4},{"user_id"=>3}])).to eq(2)
+    expect(reducer.process(extracts)).to eq(2)
   end
 end
