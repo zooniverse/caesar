ffe44a534a3cb50216102707ce4fc9a6b49c10b9
Amy Boyer <amy@zooniverse.org>
2017-03-13 14:26:45 -0500

    add extractors and reducers to get the answer for a single-question task

---
+++ app/models/extractors/question_extractor.rb
@@ -0,0 +1,13 @@
+module Extractors
+  class QuestionExtractor < Extractor
+    def process(classification)
+      classification.annotations.fetch(task_key)[0]["value"]
+    end
+
+    private
+
+    def task_key
+      config["task_key"] || "T0"
+    end
+  end
+end
---
+++ spec/models/reducers/count_choices_reducer_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Reducers::CountChoicesReducer do
+  subject(:reducer) { described_class.new("s") }
+  let(:extracts){[
+    Extract.new(
+      :classification_id => 1234,
+      :classification_at => Date.new(2017,2,5),
+      :data => "0"
+    ),
+    Extract.new(
+      :classification_id => 1234,
+      :classification_at => Date.new(2017,2,5),
+      :data => "0"
+    ),
+    Extract.new(
+      :classification_id => 1234,
+      :classification_at => Date.new(2017,2,5),
+      :data => "0"
+    ),
+    Extract.new(
+      :classification_id => 1234,
+      :classification_at => Date.new(2017,2,5),
+      :data => "1"
+    ),
+    Extract.new(
+      :classification_id => 1234,
+      :classification_at => Date.new(2017,2,5),
+      :data => "0"
+    )
+  ]}
+
+  describe '#process' do
+    it 'processes when there are no classifications' do
+      expect(reducer.process([])).to eq({})
+    end
+    it 'groups and counts the extractions' do
+      expect(reducer.process(extracts)).to eq( "0"=>4 , "1"=>1 )
+    end
+  end
+
+end
