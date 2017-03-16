1761b205c0bd9d168104b1a2daa5b3db0cd2a066
Amy Boyer <amy@zooniverse.org>
2017-03-16 10:46:40 -0500

    allow user to specify default value to be used in a lookup if no classifications have yet recorded a given key

--- app/models/conditions/lookup.rb
+++ app/models/conditions/lookup.rb
@@ -1,11 +1,12 @@
 module Conditions
   class Lookup
-    def initialize(key)
+    def initialize(key, absent_val=nil)
       @key = key
+      @absent_val = absent_val
     end
 
     def apply(bindings)
-      bindings.fetch(@key)
+      bindings.fetch(@key, @absent_val)
     end
   end
 end
--- spec/models/rule_bindings_spec.rb
+++ spec/models/rule_bindings_spec.rb
@@ -10,6 +10,20 @@ describe RuleBindings do
     expect(rule_bindings.fetch("other.b")).to eq(2)
   end
 
+  it 'handles absent keys' do
+    reductions = [
+      Reduction.new(reducer_id: 'count', data: {"a" => 1}),
+      Reduction.new(reducer_id: 'other', data: {"b" => 2})
+    ]
+
+    unexpected = double
+
+    rule_bindings = described_class.new(reductions)
+    expect(rule_bindings.fetch("count.a")).to eq(1)
+    expect(rule_bindings.fetch("count.b")).to be(nil)
+    expect(rule_bindings.fetch("count.b",unexpected)).to eq(unexpected)
+  end
+
   it 'works with overlapping data keys' do
     reductions = [
       Reduction.new(reducer_id: 'count', data: {"a" => 1}),
