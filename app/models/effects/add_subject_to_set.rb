973895c1ab82e2ebb9c973e602ea091d1547c8fb
Amy Boyer <amy@zooniverse.org>
2017-05-03 13:56:28 -0500

    rescue the exception; if it was anything but a duplicate key error, re-throw it

--- app/models/effects/add_subject_to_set.rb
+++ app/models/effects/add_subject_to_set.rb
@@ -2,10 +2,18 @@ module Effects
   class AddSubjectToSet < Effect
     def perform(workflow_id, subject_id)
       Effects.panoptes.add_subjects_to_subject_set(subject_set_id, [subject_id])
+    rescue Panoptes::Client::ServerError => se
+      # don't blow up if the subject is already in the subject set;
+      # this can happen from time to time and is OK
+      raise unless self.class.was_duplicate(se)
     end
 
     def subject_set_id
       config.fetch("subject_set_id")
+    end
+
+    def self.was_duplicate(err)
+      err.message.include? "PG::UniqueViolation"
     end
   end
 end
