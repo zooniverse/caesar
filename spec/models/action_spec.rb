b5f2e47ff87f7060be80696bd4eda8ab6887d27d
Marten Veldthuis <marten@veldthuis.com>
2017-05-04 10:46:42 +0200

    Add tests for updating action status

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Action, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform' do
+    let(:workflow) { Workflow.create! }
+    let(:subject) { Subject.create! }
+
+    it 'performs the effect' do
+      expect_any_instance_of(Effects::RetireSubject).to receive(:perform).with(workflow.id, subject.id)
+
+      action = Action.new(effect_type: 'retire_subject', workflow_id: workflow.id, subject_id: subject.id)
+      action.perform
+    end
+
+    it 'marks the action as performed' do
+      allow_any_instance_of(Effects::RetireSubject).to receive(:perform)
+
+      action = Action.new(effect_type: 'retire_subject', workflow_id: workflow.id, subject_id: subject.id)
+      action.perform
+      expect(action.status).to eq('completed')
+      expect(action.completed_at).to be_present
+    end
+  end
 end
