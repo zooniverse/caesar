e074bea250b89e912008ba10ae81a3059d6fc7de
Amy Boyer <amy@zooniverse.org>
2017-06-01 12:36:39 -0500

    write test for hooks engine

--- spec/models/webhooks/engine_spec.rb
+++ spec/models/webhooks/engine_spec.rb
@@ -1,16 +1,41 @@
 require 'spec_helper'
 
 describe Webhooks::Engine do
+  let(:nil_engine) { described_class.new(nil)}
+
+  let(:zero_engine) { described_class.new([]) }
+
+  let(:one_engine) { described_class.new([{
+   "endpoint" => "http://example.org",
+   "events" => ["extraction"]
+  }]) }
+
+  let(:two_engine) { described_class.new([{
+    "endpoint" => "http://example.org",
+    "events" => ["classification"]
+  }, {
+    "endpoint" => "http://example.org",
+    "events" => ["reduction"]
+  }]) }
+
   describe '#initialize' do
+    it 'builds itself correctly' do
+      expect(nil_engine.size).to equal(0)
+      expect(zero_engine.size).to equal(0)
+      expect(one_engine.size).to equal(1)
+      expect(two_engine.size).to equal(2)
+    end
   end
 
   describe '#process' do
-  end
+    it 'queues up notification jobs correctly' do
 
-  describe '#present?' do
-  end
-
-  describe '#size' do
+      expect do
+        two_engine.process("classification", {})
+        two_engine.process("extraction", {})
+        two_engine.process("reduction", {})
+      end.to change(NotifyWebhookWorker.jobs, :size).by(2)
+    end
   end
 
 end
