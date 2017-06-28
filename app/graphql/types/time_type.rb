9cc6ea955f48c30969a96eebafeacf3f4fcc0c15
Marten Veldthuis <marten@veldthuis.com>
2017-06-28 10:43:26 +0200

    Add documentation

--- app/graphql/types/time_type.rb
+++ app/graphql/types/time_type.rb
@@ -1,6 +1,6 @@
 Types::TimeType = GraphQL::ScalarType.define do
   name "Time"
-  description "Time since epoch in seconds"
+  description "Time since epoch in seconds (aka UNIX timestamp)."
 
   coerce_input ->(value, ctx) { Time.at(Float(value)) }
   coerce_result ->(value, ctx) { value.to_f }
--- app/graphql/types/workflow_type.rb
+++ app/graphql/types/workflow_type.rb
@@ -2,16 +2,16 @@ Types::WorkflowType = GraphQL::ObjectType.define do
   name "Workflow"
 
   field :id, !types.ID
-  field :created_at, !Types::TimeType
-  field :updated_at, !Types::TimeType
+  field :created_at, !Types::TimeType, "Timestamp when this workflow was created"
+  field :updated_at, !Types::TimeType, "Timestamp when this workflow was updated"
 
-  field :extractors_config, Types::JsonType
-  field :reducers_config, Types::JsonType
-  field :rules_config, Types::JsonType
+  field :extractors_config, Types::JsonType, "List of extractors"
+  field :reducers_config, Types::JsonType, "List of reducers"
+  field :rules_config, Types::JsonType, "List of rules"
 
   field :extracts, types[Types::ExtractType] do
-    argument :subject_id, !types.ID
-    argument :extractor_id, types.String
+    argument :subject_id, !types.ID, "Filter by specific subject"
+    argument :extractor_id, types.String, "Filter by specific extractor"
 
     resolve -> (workflow, args, ctx) {
       scope = workflow.extracts
@@ -22,7 +22,7 @@ Types::WorkflowType = GraphQL::ObjectType.define do
   end
 
   field :reductions, types[Types::ReductionType] do
-    argument :subject_id, !types.ID
+    argument :subject_id, !types.ID, "Filter by specific subject"
 
     resolve -> (workflow, args, ctx) {
       workflow.reductions.where(subject_id: args[:subject_id])
@@ -30,7 +30,7 @@ Types::WorkflowType = GraphQL::ObjectType.define do
   end
 
   field :actions, types[Types::ActionType] do
-    argument :subject_id, !types.ID
+    argument :subject_id, !types.ID, "Filter by specific subject"
 
     resolve -> (workflow, args, ctx) {
       workflow.actions.where(subject_id: args[:subject_id])
