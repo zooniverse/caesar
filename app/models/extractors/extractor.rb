a8fe7a01d421364bb761df608122b06b4de85ae9
Amy Boyer <amy@zooniverse.org>
2017-08-31 14:32:28 -0500

    extractor ignores classifications if they are older than a specified minumum_version

--- app/models/extractors/extractor.rb
+++ app/models/extractors/extractor.rb
@@ -2,7 +2,7 @@ module Extractors
   class Extractor
     include Configurable
 
-    attr_reader :key, :config
+    attr_reader :key, :config, :min_version
 
     @@NoData = Object.new
     def self.NoData
@@ -10,12 +10,20 @@ module Extractors
     end
 
     def process(classification)
+      return Extractor.NoData if too_old?(classification)
       extract_data_for(classification)
     end
 
     def initialize(key, config = {})
       @key = key
+      @min_version = config["minimum_version"] || nil
       load_configuration(config)
+    end
+
+    private
+
+    def too_old?(classification)
+      Gem::Version.new(min_version) > Gem::Version.new(classification.workflow_version)
     end
   end
 end
