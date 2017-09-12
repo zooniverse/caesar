cfa07bee29079a53a5d34b06c13bf3a4a70262ac
Marten Veldthuis <marten@veldthuis.com>
2017-09-12 13:07:35 +0100

    Allow question extractor to skip missing annotations

--- app/models/extractors/question_extractor.rb
+++ app/models/extractors/question_extractor.rb
@@ -1,10 +1,13 @@
 module Extractors
   class QuestionExtractor < Extractor
+    class MissingAnnotation < StandardError; end
+
     validates :task_key, presence: true
+    validates :if_missing, presence: true, inclusion: {in: ["error", "ignore"]}
 
     def extract_data_for(classification)
       CountingHash.build do |result|
-        classification.annotations.fetch(task_key).each do |annotation|
+        fetch_annotations(classification).each do |annotation|
           value = annotation.fetch("value")
 
           case value
@@ -19,8 +22,29 @@ module Extractors
 
     private
 
+    def fetch_annotations(classification)
+      case if_missing
+      when "error"
+        begin
+          classification.annotations.fetch(task_key)
+        rescue KeyError
+          raise MissingAnnotation, "No annotations for task #{task_key}"
+        end
+      when "ignore"
+        begin
+          classification.annotations.fetch(task_key)
+        rescue KeyError
+          []
+        end
+      end
+    end
+
     def task_key
       config.fetch("task_key", "T0")
+    end
+
+    def if_missing
+      config["if_missing"] || "error"
     end
   end
 end
