3baefb041fd719b9568f617d0e740a0fb680f095
Marten Veldthuis <marten@veldthuis.com>
2017-09-25 12:29:30 +0200

    Make sure empty version is persisted as nil

--- app/models/extractor.rb
+++ app/models/extractor.rb
@@ -1,8 +1,29 @@
 class Extractor < ApplicationRecord
+  def self.of_type(type)
+    case type.to_s
+    when "blank"
+      Extractors::BlankExtractor
+    when "external"
+      Extractors::ExternalExtractor
+    when "question"
+      Extractors::QuestionExtractor
+    when "survey"
+      Extractors::SurveyExtractor
+    when "who"
+      Extractors::WhoExtractor
+    when "pluck_field"
+      Extractors::PluckFieldExtractor
+    else
+      raise "Unknown type #{type}"
+    end
+  end
+
   belongs_to :workflow
 
   validates :workflow, presence: true
   validates :key, presence: true, uniqueness: {scope: [:workflow_id]}
+
+  before_validation :nilify_empty_fields
 
   NoData = Class.new
 
@@ -22,22 +43,7 @@ class Extractor < ApplicationRecord
     Gem::Version.new(minimum_workflow_version) > Gem::Version.new(classification.workflow_version)
   end
 
-  def self.of_type(type)
-    case type.to_s
-    when "blank"
-      Extractors::BlankExtractor
-    when "external"
-      Extractors::ExternalExtractor
-    when "question"
-      Extractors::QuestionExtractor
-    when "survey"
-      Extractors::SurveyExtractor
-    when "who"
-      Extractors::WhoExtractor
-    when "pluck_field"
-      Extractors::PluckFieldExtractor
-    else
-      raise "Unknown type #{type}"
-    end
+  def nilify_empty_fields
+    self.minimum_workflow_version = nil if minimum_workflow_version.empty?
   end
 end
