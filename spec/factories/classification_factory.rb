010f6699d5a1b5c5f66a226bf956b5f397f85806
Marten Veldthuis <marten@veldthuis.com>
2017-10-18 18:22:20 +0100

    Use factory for more things

--- spec/factories/classification_factory.rb
+++ spec/factories/classification_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :classification do
     id { generate :classification_id }
-    project_id { workflow.project_id }
+    project_id { workflow&.project_id || 1 }
     workflow_version "1.1"
   end
 end
--- spec/models/extractors/who_extractor_spec.rb
+++ spec/models/extractors/who_extractor_spec.rb
@@ -2,18 +2,7 @@ require 'spec_helper'
 
 describe Extractors::WhoExtractor do
   let(:extractor){ described_class.new(key: "s") }
-  let(:classification){
-    # Classification.new({userId: 5, links: []})
-    Classification.new(
-      "links"=>{
-        "project"=>"2439",
-        "user"=>"5",
-        "workflow"=>"1234",
-        "workflow_content"=>"1716",
-        "subjects"=>"4567"
-      }
-    )
-  }
+  let(:classification) { build :classification, user_id: 5 }
 
   it 'gives the user who performed the classification' do
     expect(extractor.process(classification)).to eq({"user_id" => 5})
