75f96bd2302fc96f23aeeccb8e71ad4fc43baece
Marten Veldthuis <marten@veldthuis.com>
2017-10-20 13:02:03 +0100

    Add filter for empty extracts

--- spec/models/extract_filter_spec.rb
+++ spec/models/extract_filter_spec.rb
@@ -41,6 +41,14 @@ describe ExtractFilter do
     ]
   }
 
+  describe 'with no filters' do
+    let(:filter) { ExtractFilter.new({}) }
+
+    it 'returns the unfiltered list of extracts, sorted by classification_at' do
+      expect(filter.filter(extracts)).to eq([extracts[2], extracts[0], extracts[1], extracts[3], extracts[4]])
+    end
+  end
+
   describe 'extractor AND subrange filtering' do
     it 'filters by extractor first' do
       extracts = [
@@ -53,14 +61,6 @@ describe ExtractFilter do
       # extract produced by the 'bar' extractor
       filter = described_class.new(from: 0, to: 0, extractor_keys: ["bar"])
       expect(filter.filter(extracts)).not_to be_empty
-    end
-  end
-
-  describe 'with no filters' do
-    let(:filter) { ExtractFilter.new({}) }
-
-    it 'returns the unfiltered list of extracts, sorted by classification_at' do
-      expect(filter.filter(extracts)).to eq([extracts[2], extracts[0], extracts[1], extracts[3], extracts[4]])
     end
   end
 
@@ -90,6 +90,42 @@ describe ExtractFilter do
     it 'returns extracts from the given extractor' do
       filter = described_class.new(extractor_keys: ["foo"])
       expect(filter.filter(extracts)).to eq([extracts[0], extracts[1], extracts[4]])
+    end
+  end
+
+  describe 'empty filtering' do
+    it 'returns all extracts when set to keep_all' do
+      extracts = [
+        build(:extract, data: {}),
+        build(:extract, data: {a: 1})
+      ]
+
+      filter = described_class.new(empty_extracts: "keep_all")
+      expect(filter).to be_valid
+      expect(filter.filter(extracts)).to eq(extracts)
+    end
+
+    it 'returns only non-empty extracts when set to ignore_empty' do
+      extracts = [
+        build(:extract, data: {}),
+        build(:extract, data: {a: 1})
+      ]
+
+      filter = described_class.new(empty_extracts: "ignore_empty")
+      expect(filter).to be_valid
+      expect(filter.filter(extracts)).to eq([extracts[1]])
+    end
+
+    it 'filters repeats before filtering emptyness' do
+      extracts = [
+        build(:extract, user_id: 1, classification_at: 5.minutes.ago, data: {a: 1}),
+        build(:extract, user_id: 1, classification_at: 2.minutes.ago, data: {}),
+        build(:extract, user_id: 2, classification_at: 1.minutes.ago, data: {a: 1})
+      ]
+
+      filter = described_class.new(empty_extracts: "ignore_empty", repeated_classifications: "keep_last")
+      expect(filter).to be_valid
+      expect(filter.filter(extracts)).to eq([extracts[2]])
     end
   end
 
