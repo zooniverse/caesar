9757ad426a0ee3ef9ba5ae334105bd3e83e83287
Adam McMaster <adam@zooniverse.org>
2018-01-04 14:41:44 +0000

    Fix startup errors

--- docker/start-puma.sh
+++ docker/start-puma.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-cd /rails_app
-
 if [ -d "/rails_conf/" ]
 then
     ln -sf /rails_conf/* ./config/
@@ -10,7 +8,7 @@ fi
 mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
-if [ "$RAILS_ENV" -ne "development" ]; then
+if [ "$RAILS_ENV" != "development" ]; then
   USER_DATA=$(curl --fail http://169.254.169.254/latest/user-data || echo "")
 
   if [ "$USER_DATA" == "EMERGENCY_MODE" ]
