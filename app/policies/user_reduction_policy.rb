32aa06b93e2cd3c6f97fa035e11a2039d0a82406
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2018-06-21 17:24:52 -0500

    Temp update user reduction policy

--- app/policies/user_reduction_policy.rb
+++ app/policies/user_reduction_policy.rb
@@ -3,8 +3,11 @@ class UserReductionPolicy < ApplicationPolicy
     def resolve
       workflow_ids = Pundit.policy_scope!(credential, Workflow).pluck(:id)
 
-      scope = self.scope.joins(:workflow).references(:workflows)
-      scope.where(workflow_id: workflow_ids).or(scope.where(workflows: {public_reductions: true}))
+      # scope = self.scope.joins(:workflow).references(:workflows)
+      # scope.where(workflow_id: workflow_ids).or(scope.where(workflows: {public_reductions: true}))
+
+      # Temporary, see subject_reduction_policy.rb
+      scope = self.scope.where(reducible_id: workflow_ids)
     end
   end
 
@@ -14,7 +17,7 @@ class UserReductionPolicy < ApplicationPolicy
 
   def update?
     return true if credential.admin?
-    credential.project_ids.include?(record.workflow.project_id)
+    credential.project_ids.include?(record.reducible.project_id)
   end
 
   def destroy?
