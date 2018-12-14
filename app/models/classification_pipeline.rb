3c68b1c54be5dfadcb980b50b920f48cad5cdb65
Marten Veldthuis <marten@veldthuis.com>
2018-12-14 11:30:35 +0000

    Merge ClassificationPipeline into IsReducible

--- app/models/classification_pipeline.rb
+++ app/models/classification_pipeline.rb
@@ -1,9 +1,2 @@
 module ClassificationPipeline
-  def reducers_runner
-    RunsReducers.new(self, reducers)
-  end
-
-  def rules_runner
-    RunsRules.new(self, subject_rules.rank(:row_order), user_rules.rank(:row_order), rules_applied)
-  end
 end
