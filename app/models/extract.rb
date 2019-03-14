a9f26f98bb4232ea71493017853df820f2a4bab8
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-03-14 13:15:19 -0500

    Include relevant reduction in jsonified extracts

--- app/models/extract.rb
+++ app/models/extract.rb
@@ -22,4 +22,8 @@ class Extract < ApplicationRecord
   belongs_to :subject
   has_and_belongs_to_many_with_deferred_save :subject_reduction
   has_and_belongs_to_many_with_deferred_save :user_reduction
+
+  def as_json(*)
+    super(methods: :relevant_reduction)
+  end
 end
