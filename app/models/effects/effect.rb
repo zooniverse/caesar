0203c33fa4cfb35fd57a5dbfc60a3a38bd1bbe8c
Amy Boyer <amy@zooniverse.org>
2019-05-09 13:26:59 -0500

    keys don't come back from the DB as symbols

--- app/models/effects/effect.rb
+++ app/models/effects/effect.rb
@@ -1,6 +1,12 @@
 module Effects
   class Effect
-    attr_reader :config
+    def config=(new_config)
+      @config = new_config
+    end
+
+    def config
+      @config.with_indifferent_access
+    end
 
     def initialize(config = {})
       @config = config
