18e417fc7223e96e588bac076d06760b98f06817
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-05-30 13:01:04 -0500

    Apply indexes concurrently

--- db/migrate/20190530163752_add_lock_version_index_to_reductions.rb
+++ db/migrate/20190530163752_add_lock_version_index_to_reductions.rb
@@ -1,19 +1,6 @@
 class AddLockVersionIndexToReductions < ActiveRecord::Migration[5.2]
   def change
-    if index_exists?(:user_reductions, [:id, :lock_version])
-      remove_index :user_reductions, [:id, :lock_version]
-    end
-
-    unless index_exists?(:user_reductions, [:id, :lock_version])
-      add_index :user_reductions, [:id, :lock_version]
-    end
-
-    if index_exists?(:subject_reductions, [:id, :lock_version])
-      remove_index :subject_reductions, [:id, :lock_version]
-    end
-
-    unless index_exists?(:subject_reductions, [:id, :lock_version])
-      add_index :subject_reductions, [:id, :lock_version]
-    end
+    add_index :user_reductions, [:id, :lock_version], unique: true, algorithm: :concurrently
+    add_index :subject_reductions, [:id, :lock_version], unique: true, algorithm: :concurrently
   end
 end
