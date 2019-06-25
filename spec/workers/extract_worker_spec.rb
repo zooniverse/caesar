d40991e823eed61b0b6183f56914d572bbcbb367
Amy Boyer <amyrebecca@users.noreply.github.com>
2019-06-25 11:35:28 -0500

    delete classifications asynchronously (#851)
    * delete classifications asynchronously
    
    * consolidate specs

--- spec/workers/extract_worker_spec.rb
+++ spec/workers/extract_worker_spec.rb
@@ -9,24 +9,18 @@ RSpec.describe ExtractWorker, type: :worker do
     described_class.new.perform(classification.id)
   end
 
-  it 'marks the classification as processed' do
+  it 'queues the classification to be deleted' do
     classification = create :classification, workflow: workflow, subject: subject
     expect do
       described_class.new.perform(classification.id)
-    end.to change { Classification.count }.from(1).to(0)
+    end.to change(DeleteClassificationWorker.jobs, :size).by(1)
   end
 
   context 'when classification is not in DB' do
-    it 'ignores error if extract exists' do
+    it 'ignores error' do
       extract = create :extract
       expect do
         described_class.new.perform(extract.classification_id)
-      end.not_to raise_error
-    end
-
-    it 'no longer raises error even if no extracts exist' do
-      expect do
-        described_class.new.perform(-1)
       end.not_to raise_error
     end
   end
