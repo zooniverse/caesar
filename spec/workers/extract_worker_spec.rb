947fad4483e2367514a64d3a76713959493fef5a
Amy Boyer <amy@zooniverse.org>
2019-06-25 11:22:59 -0500

    consolidate specs

--- spec/workers/extract_worker_spec.rb
+++ spec/workers/extract_worker_spec.rb
@@ -17,16 +17,10 @@ RSpec.describe ExtractWorker, type: :worker do
   end
 
   context 'when classification is not in DB' do
-    it 'ignores error if extract exists' do
+    it 'ignores error' do
       extract = create :extract
       expect do
         described_class.new.perform(extract.classification_id)
-      end.not_to raise_error
-    end
-
-    it 'no longer raises error even if no extracts exist' do
-      expect do
-        described_class.new.perform(-1)
       end.not_to raise_error
     end
   end
