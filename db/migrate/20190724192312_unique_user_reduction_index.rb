27deeea09ef3508692734f70d136aadc44ba5257
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-08-20 16:42:58 -0500

    Avoid rebuilding existing index

--- db/migrate/20190724192312_unique_user_reduction_index.rb
+++ db/migrate/20190724192312_unique_user_reduction_index.rb
@@ -2,13 +2,14 @@ class UniqueUserReductionIndex < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    remove_index :user_reductions, name: "index_user_reductions_covering"
 
-    add_index :user_reductions,
-      [:reducible_id, :reducible_type, :user_id, :reducer_key, :subgroup],
-      name: "index_user_reductions_covering",
-      unique: true,
-      algorithm: :concurrently
+    unless index_exists?(:user_reductions, [:reducible_id, :reducible_type, :user_id, :reducer_key, :subgroup]) do
+      add_index :user_reductions,
+        [:reducible_id, :reducible_type, :user_id, :reducer_key, :subgroup],
+        name: "index_user_reductions_covering",
+        unique: true,
+        algorithm: :concurrently
+    end
 
     add_index :user_reductions,
       [:reducible_id, :reducible_type, :user_id],
