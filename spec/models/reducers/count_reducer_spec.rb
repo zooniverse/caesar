91f3c3de66560412a0cfe0cf589eeb37544fc720
Campbell Allen <campbell.allen@gmail.com>
2019-10-22 16:28:08 +0100

    remove invalid spec
    this count reducer never implemented this behaviour and the spec was incorrectly setup, for an evergreen pass :(

--- spec/models/reducers/count_reducer_spec.rb
+++ spec/models/reducers/count_reducer_spec.rb
@@ -23,15 +23,6 @@ describe Reducers::CountReducer do
     ], build(:subject_reduction)).data).to include('extracts' => 4)
   end
 
-  it 'ignores existing reduction data in default mode', :focus do
-    default_reducer = described_class.new(reduction_mode: Reducer.reduction_modes[:default_reduction])
-    reduction = build :subject_reduction, reducer_key: 'data', subgroup: '_default', data: { 'extracts' => 3, 'classifications' => 1 }
-
-    result = default_reducer.reduce_into([Extract.new(classification_id: 1), Extract.new(classification_id: 1), Extract.new(classification_id: 2)], reduction)
-    expect(result.data["classifications"]).to eq(2)
-    expect(result.data["extracts"]).to eq(3)
-  end
-
   it 'uses existing reduction data in running aggregation mode' do
     running_reducer = described_class.new(reduction_mode: Reducer.reduction_modes[:running_reduction])
     reduction = build :subject_reduction, reducer_key: 'data', subgroup: '_default', data: { 'extracts' => 3, 'classifications' => 1 }
