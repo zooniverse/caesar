188e35ec97a8f3b768ed2062925401b501ddecd1
lcjohnso <lcj@northwestern.edu>
2019-11-01 20:17:52 -0500

    add retirement reason choices

--- app/models/effects/retire_subject.rb
+++ app/models/effects/retire_subject.rb
@@ -1,5 +1,8 @@
 module Effects
   class RetireSubject < Effect
+
+    CONFIG_CHOICES = ["classification_count", "flagged", "nothing_here", "blank", "consensus", "other", "human"]
+
     def perform(workflow_id, subject_id)
       Effects.panoptes.retire_subject(workflow_id, subject_id, reason: reason)
     end
@@ -9,7 +12,7 @@ module Effects
     end
 
     def self.config_fields
-      [:reason].freeze
+      [[:reason, CONFIG_CHOICES]].freeze
     end
 
     def reason
