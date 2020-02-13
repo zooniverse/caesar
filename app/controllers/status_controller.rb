17d6b011b69e469a2e7554af40d9472be4fb96a2
Campbell Allen <campbell.allen@gmail.com>
2020-02-13 16:26:07 +0000

    add caching to status page
    avoid costly queries every time this is hit use a cache for reuse

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,9 +1,22 @@
+# frozen_string_literal: true
 class StatusController < ApplicationController
   skip_before_action :authenticate!
 
   def show
     skip_authorization
-    @status = ApplicationStatus.new
+    @status = Rails.cache.fetch(status_cache_key, expires_in: status_cache_ttl) do
+      ApplicationStatus.new
+    end
     respond_with @status
+  end
+
+  private
+
+  def status_cache_ttl
+    ENV.fetch('STATUS_CACHE_TTL', 1.minute)
+  end
+
+  def status_cache_key
+    ENV.fetch('STATUS_CACHE_KEY', 'app-status')
   end
 end
