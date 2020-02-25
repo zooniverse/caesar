44681bd5989c343e38853c30fc2d7238afe8c44e
Campbell Allen <campbell.allen@gmail.com>
2020-02-25 11:23:18 +0000

    remove focus from spec

--- spec/lib/error_logger_spec.rb
+++ spec/lib/error_logger_spec.rb
@@ -5,7 +5,7 @@ describe ErrorLogger do
   describe '.report' do
     let(:exception) { Exception.new('testing exception') }
 
-    it 'calls raven (sentry) to log the error', :focus do
+    it 'calls raven (sentry) to log the error' do
       allow(Raven).to receive(:capture_exception)
       ErrorLogger.report(exception)
       expect(Raven).to have_received(:capture_exception).with(exception)
