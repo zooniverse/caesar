9cf73d50b40260eacf4a02874e72e913db4ea124
Campbell Allen <campbell.allen@gmail.com>
2020-06-18 15:06:36 +0100

    use sidekiq config yml (#1162)
    use consistent mechanism to configure sidekiq via env vars

--- docker/start-sidekiq.sh
+++ docker/start-sidekiq.sh
@@ -6,6 +6,4 @@ set -ex
 mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
-# allow the sidekiq args to come via the env variables
-CLI_ARGS=${SIDEKIQ_ARGS:-''}
-exec bundle exec sidekiq $CLI_ARGS
+exec bundle exec sidekiq -C config/sidekiq.yml
