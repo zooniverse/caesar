0b80aa6a4d2574772dab3c3a59b27dea0a435295
Campbell Allen <campbell.allen@gmail.com>
2020-06-18 18:16:45 +0100

    add comment on why cli args override

--- docker/start-sidekiq.sh
+++ docker/start-sidekiq.sh
@@ -7,5 +7,6 @@ mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
 # allow the sidekiq args to come via the env variables
+# and is needed for overriding sidekiq config file i.e. for custom tess queue processing
 CLI_ARGS=${SIDEKIQ_ARGS:-''}
 exec bundle exec sidekiq $CLI_ARGS
