8eb6ad705a3cfc9252481f9fdcc97a9ececfe87b
Campbell Allen <campbell.allen@gmail.com>
2020-06-25 13:22:10 +0100

    do not report rest client errors to sentry (#1172)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 Raven.configure do |config|
+  # https://docs.sentry.io/clients/ruby/config/
   config.dsn = ENV['SENTRY_DSN']
 
   config.current_environment = ENV['SENTRY_ENV'] || Rails.env
   config.sanitize_fields = ["credentials"]
+
+  config.excluded_exceptions << 'RestClient::BadRequest'
 end
