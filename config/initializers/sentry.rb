fa506307b03b47b92824f2a8d0104c64ed71492c
Campbell Allen <campbell.allen@gmail.com>
2020-06-25 10:16:36 +0100

    Revert "do not report rest client errors to sentry"
    This reverts commit 5ba8c6f9734934e4c3cdc4ca167d0a6628130af4.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,6 @@
-# frozen_string_literal: true
-
 Raven.configure do |config|
-  # https://docs.sentry.io/clients/ruby/config/
   config.dsn = ENV['SENTRY_DSN']
 
   config.current_environment = ENV['SENTRY_ENV'] || Rails.env
   config.sanitize_fields = ["credentials"]
-
-  config.excluded_exceptions << 'RestClient::BadRequest'
 end
