0c0c9234a5c63d4f78557797ff84108a9e9f49b5
Campbell Allen <campbell.allen@gmail.com>
2021-06-22 12:35:19 +0100

    Fix app puma boot and AR connection pool config for tess sidekiq (#1321)
    * ensure db pool is int
    
    * assign port to local var and then bind

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,8 @@ pidfile "#{app_path}/tmp/pids/server.pid"
 state_path "#{app_path}/tmp/pids/puma.state"
 
 environment ENV.fetch('RAILS_ENV', 'development')
-port        ENV.fetch('PORT', 3000)
 
+port = ENV.fetch('PORT', 3000)
 bind "tcp://0.0.0.0:#{port}"
 
 threads_count = ENV.fetch('RAILS_MAX_THREADS', 2).to_i
