cddfc4cf02747e6456d371be01d41f32f20c5d37
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-08-24 20:02:14 -0500

    Update logic into worker if large amounts of data

--- app/workers/create_extracts_worker.rb
+++ app/workers/create_extracts_worker.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
+
+# Worker to bulk import extracts from csv
 class CreateExtractsWorker
   include Sidekiq::Worker
+  BATCH_SIZE = 100
 
-  def perform(extracts)
+  def perform(csv_filepath, workflow_id)
+    @bulk_extracts = []
+    CSV.foreach(csv_filepath, headers: true, header_converters: :symbol) do |row|
+      extract = row.to_hash
+      extract[:workflow_id] = workflow_id unless workflow_id.nil?
+      extract[:classification_at] = Time.now
+      extract[:classification_id] = workflow_id
+      @bulk_extracts << extract
+      bulk_enqueue_current_batch if @bulk_extracts.length > BATCH_SIZE
+    end
+    bulk_enqueue_current_batch
+  end
+
+  private
+
+  def bulk_enqueue_current_batch
     Extract.import extracts, validate: false
+    @bulk_extracts = []
   end
 end
