035624c6a79d78760d6f294bea6ba22cdaedbc2f
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-08-24 20:49:57 -0500

    update worker with notes and typo for draft pr review

--- app/workers/create_extracts_worker.rb
+++ app/workers/create_extracts_worker.rb
@@ -10,9 +10,14 @@ class CreateExtractsWorker
     @bulk_extracts = []
     CSV.foreach(csv_filepath, headers: true, header_converters: :symbol) do |row|
       extract = row.to_hash
-      extract[:workflow_id] = workflow_id unless workflow_id.nil?
+      # possible that csv will include data for multiple workflows?
+      extract[:workflow_id] = workflow_id if row.to_hash[:workflow_id].nil?
       extract[:classification_at] = Time.now
-      extract[:classification_id] = workflow_id
+      # testing with ANY int id to bypass not null constraint, but strongly
+      # thinking of removing not nulll constraint for classification id and
+      # doing a condition unique constraint for extractor_key,
+      # classification_id combo only when classification id is not null
+      extract[:classification_id] = 0 if row.to_hash[:classification_id].nil?
       @bulk_extracts << extract
       bulk_enqueue_current_batch if @bulk_extracts.length > BATCH_SIZE
     end
