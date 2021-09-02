5b32a860a334f96918964a22120dbd77779d9fe5
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-09-02 13:21:11 -0500

    update to upsert ml extracts

--- app/workers/create_extracts_worker.rb
+++ app/workers/create_extracts_worker.rb
@@ -1,27 +1,48 @@
 # frozen_string_literal: true
 
 require 'csv'
-# Worker to bulk import extracts from csv
+# Worker to bulk import machine learnt extracts from web accessibly located csv
 class CreateExtractsWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 2
 
   def perform(csv_filepath, workflow_id)
-    @bulk_extracts = []
-    init_extracts csv_filepath, workflow_id
-    Extract.import @bulk_extracts, validate: false, all_or_none: true
+    upsert_extracts csv_filepath, workflow_id
+    # todo run Reducers check if worklow has reducers 
+    # if !result.failed_instances.empty?
+    # send email
   end
 
   private
 
-  def init_extracts(csv_filepath, workflow_id)
+  def upsert_extracts(csv_filepath, workflow_id)
     UrlDownloader.stream(csv_filepath) do |io|
       csv = CSV.new(io, headers: true)
       csv.each do |row|
-        extract = row.to_hash
-        extract[:workflow_id] = workflow_id
-        extract[:classification_at] = Time.now
-        @bulk_extracts << extract
+        hashed_row = row.to_hash
+        extract = init_extract hashed_row, workflow_id
+        extract.save!
       end
     end
+  end
+
+  def init_extract(hashed_row, workflow_id)
+    extract = Extract.where(
+      workflow_id: workflow_id,
+      subject_id: hashed_row['subject_id'],
+      extractor_key: hashed_row['extractor_key'],
+      workflow_version: hashed_row['workflow_version'],
+      project_id: workflow(workflow_id).project_id,
+      machine_data: true,
+      classification_id: nil
+    ).first_or_initialize
+
+    extract.data = hashed_row['data']
+    extract.classification_at = Time.now unless extract.classification_at.present?
+    extract
+  end
+
+  def workflow(workflow_id)
+    Workflow.find(workflow_id)
   end
 end
--- spec/workers/create_extracts_worker_spec.rb
+++ spec/workers/create_extracts_worker_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 RSpec.describe CreateExtractsWorker, type: :worker do
   let(:workflow) { create :workflow }
 
-  describe ''
+  describe '#perform' do
+    
+  end
 end
