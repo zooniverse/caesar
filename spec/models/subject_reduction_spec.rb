14435da5edda11380001024138be1f5c1397fb49
Campbell Allen <campbell.allen@gmail.com>
2021-09-09 13:27:59 +0100

    add reducer_key identifier to subject_reduction payload
    ensure we can identify which reducer created this subject_reduction payload.
    
    this will be used by externall apps to link the reducer / worklow to the payload when the workflow has multiple reducers that post to an end point.

--- spec/models/subject_reduction_spec.rb
+++ spec/models/subject_reduction_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe SubjectReduction, type: :model do
     expect(build(:subject_reduction)).to be_valid
   end
 
+  it '#prepare' do
+    extract1 = create(:extract, user_id: 1)
+    reduction = create(:subject_reduction, extracts: [extract1])
+    prepared = reduction.prepare
+    expected_attributes = %w[id data subject reducer_key created_at updated_at]
+    expected_payload = reduction.attributes.slice(*expected_attributes)
+    # add the formatted payload attributes before testing
+    expected_payload['reducible'] = { 'id' => reduction.reducible_id, 'type' => reduction.reducible_type }
+    expected_payload['subject'] = reduction.subject.attributes
+    expect(expected_payload).to include(prepared)
+  end
+
   it "should prepare auxiliary attributes" do
     extract1 = create(:extract, user_id: 1)
     extract2 = create(:extract, user_id: 2)
