37e13d2283d59696c6384a4b34e44db5c68deb42
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-09-09 09:16:05 -0500

    Update create_extracts_worker.rb

--- app/workers/create_extracts_worker.rb
+++ app/workers/create_extracts_worker.rb
@@ -7,10 +7,11 @@ class CreateExtractsWorker
   sidekiq_options retry: 2
 
   def perform(csv_filepath, workflow_id)
-    upsert_extracts csv_filepath, workflow_id
-    # todo run Reducers check if worklow has reducers 
-    # if !result.failed_instances.empty?
-    # send email
+    workflow = Workflow.find(workflow_id)
+    upsert_extracts csv_filepath, workflow
+    run_workflow_reducers(workflow) if workflow&.reducers&.any?
+    project = Project.find(workflow.project_id)
+    run_project_reducers(project) if project&.has_reducers?
   end
 
   private
@@ -21,28 +22,32 @@ class CreateExtractsWorker
       csv.each do |row|
         hashed_row = row.to_hash
         extract = init_extract hashed_row, workflow_id
+        extract.data = hashed_row['data']
+        extract.classification_at = Time.now unless extract.classification_at.present?
         extract.save!
+        puts extract
       end
     end
   end
 
-  def init_extract(hashed_row, workflow_id)
-    extract = Extract.where(
-      workflow_id: workflow_id,
+  def init_extract(hashed_row, workflow)
+    Extract.where(
+      workflow_id: workflow.id,
       subject_id: hashed_row['subject_id'],
       extractor_key: hashed_row['extractor_key'],
       workflow_version: hashed_row['workflow_version'],
-      project_id: workflow(workflow_id).project_id,
+      project_id: workflow.project_id,
       machine_data: true,
       classification_id: nil
     ).first_or_initialize
-
-    extract.data = hashed_row['data']
-    extract.classification_at = Time.now unless extract.classification_at.present?
-    extract
   end
 
-  def workflow(workflow_id)
-    Workflow.find(workflow_id)
+  def run_workflow_reducers(workflow)
+    puts "MDY114 REDUCERS!"
+    workflow.rerun_reducers
+  end
+
+  def run_project_reducers(project)
+    project.rerun_reducers
   end
 end
