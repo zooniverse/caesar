4935b96a828b051f0d1556edf12b0f20e2e0f91b
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2021-09-13 14:20:47 -0500

    Add newly required initializer configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,26 @@
 require 'sidekiq/web'
+require "sidekiq-unique-jobs"
 Sidekiq::Logstash.setup
+
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_URL"] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+end
