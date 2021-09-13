2d3c7accbe24e590815dece986ab9cd063c89814
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2021-09-13 15:53:10 -0500

    Hound watcheth over me, arf bark

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,9 @@
 require 'sidekiq/web'
-require "sidekiq-unique-jobs"
+require 'sidekiq-unique-jobs'
 Sidekiq::Logstash.setup
 
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"] }
+  config.redis = { url: ENV['REDIS_URL'] }
 
   config.client_middleware do |chain|
     chain.add SidekiqUniqueJobs::Middleware::Client
@@ -18,7 +17,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"] }
+  config.redis = { url: ENV['REDIS_URL'] }
 
   config.client_middleware do |chain|
     chain.add SidekiqUniqueJobs::Middleware::Client
