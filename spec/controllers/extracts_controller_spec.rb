5a837e146d4a3afba541ca678050486a7a4240ef
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-10-01 11:58:40 -0500

    Update extracts_controller_spec.rb

--- spec/controllers/extracts_controller_spec.rb
+++ spec/controllers/extracts_controller_spec.rb
@@ -6,10 +6,24 @@ RSpec.describe ExtractsController, type: :controller do
   let(:extractor) { build(:external_extractor, key: 'ext') }
   let(:workflow) { create(:workflow, extractors: [extractor]) }
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index, params: {workflow_id: workflow.id, extractor_key: extractor.key}
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#import' do
+    file_path = 'test.csv'
+    it 'returns a 204 status' do 
+      post :import, params: { file: file_path, workflow_id: workflow.id }
+      expect(response.status).to eq(204)
+    end
+
+    it 'queues an import worker' do
+      allow(CreateExtractsWorker).to receive(:perform_async).with(file_path, workflow.id.to_s)
+      post :import, params: { file: file_path, workflow_id: workflow.id }
+      expect(CreateExtractsWorker).to have_received(:perform_async).with(file_path, workflow.id.to_s)
     end
   end
 end
