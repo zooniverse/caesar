6b00931a11e781fb34f3e7d383a3ba4d3c19575f
Campbell Allen <campbell.allen@gmail.com>
2022-03-09 16:48:46 +0000

    Add external effect with basic authentication HTTP headers (#1374)
    * add external_with_basic_auth effect
    
    used for posting user_reductions to a known target with authentication
    
    * refactor external effect
    
    * add external effect with basic auth
    
    this inherts from the external effect but instead it adds basic auth via HTTP party as it correctly uses the AUTHORIZATION header vs rest-client which adds them to the URL
    
    * ensure the external_with_basic_auth effect is valid
    
    * fix linter errors
    
    * add external_with_basic_auth effect selection to UI
    
    allow folks to wire up the new effect via the caesar UI

---
+++ app/models/effects/external_with_basic_auth.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Effects
+  class ExternalWithBasicAuth < External
+    def self.config_fields
+      @config_fields ||= %i[url reducer_key username password].freeze
+    end
+
+    def valid?
+      super && username.present? && password.present?
+    end
+
+    def username
+      config[:username]
+    end
+
+    def password
+      config[:password]
+    end
+
+    private
+
+    def post_payload_to_url
+      options = {
+        body: reduction_payload,
+        basic_auth: { username: username, password: password },
+        headers: post_request_headers
+      }
+      response = HTTParty.post(url, options)
+      # success is a 200, 201 or 204 response code - allow some leeway with the target endpoint
+      return response if response.ok? || response.created? || response.no_content?
+
+      raise ExternalEffectFailed
+    end
+
+    def post_request_headers
+      { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    end
+  end
+end
---
+++ spec/models/effects/external_with_basic_auth_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Effects::ExternalWithBasicAuth do
+  let(:subject) { create(:subject) }
+  let(:reduction) { create(:subject_reduction, reducer_key: 'key', subject: subject, data: {}) }
+  let(:url) { 'https://example.org/post/reduction/here' }
+  let(:username) { 'sloan-api' }
+  let(:password) { 'sloan-api-password' }
+  let(:effect_config) { { url: url, reducer_key: 'key', username: username, password: password } }
+  let(:effect) { described_class.new(effect_config) }
+
+  before do
+    stub_request(:post, url)
+      .with(
+        basic_auth: [username, password],
+        headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      )
+      .to_return(status: 200, headers: {})
+  end
+
+  it 'sends the reductions to the external API' do
+    effect.perform(reduction.workflow_id, reduction.subject_id)
+    expect(
+      a_request(:post, url).with(body: reduction.prepare.to_json)
+    ).to have_been_made.once
+  end
+
+  it 'raises an error if more than one reduction is matched' do
+    create(:subject_reduction, reducible: reduction.reducible, subgroup: 'lol', reducer_key: 'key', subject: subject, data: {})
+    expect do
+      effect.perform(reduction.workflow_id, reduction.subject_id)
+    end.to raise_error(Effects::ExternalWithBasicAuth::ExternalEffectFailed)
+  end
+
+  it 'raises an error if the post fails' do
+    stub_request(:post, url)
+      .with(
+        basic_auth: [username, password],
+        headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      )
+      .to_return(status: 500, headers: {})
+
+    expect do
+      effect.perform(reduction.workflow_id, reduction.subject_id)
+    end.to raise_error(Effects::ExternalWithBasicAuth::ExternalEffectFailed)
+  end
+
+  it 'does not post if no url is configured' do
+    effect = described_class.new(url: nil)
+
+    expect do
+      effect.perform(reduction.workflow_id, reduction.subject_id)
+    end.to raise_error(Effects::ExternalWithBasicAuth::InvalidConfiguration)
+  end
+
+  describe 'config' do
+    it 'does not care about symbol keys' do
+      effect = described_class.new('url': 'https://www.google.com')
+      expect(effect.url).not_to be(nil)
+      effect = described_class.new(url: 'https://www.google.com')
+      expect(effect.url).not_to be(nil)
+    end
+  end
+
+  describe 'validations' do
+    it 'is invalid with a non-https url' do
+      effect_config[:url] = 'http://foo.com'
+      effect = described_class.new(effect_config)
+      expect(effect).not_to be_valid
+    end
+
+    it 'is invalid with some strange url' do
+      effect_config[:url] = 'https:\\foo+3'
+      effect = described_class.new(effect_config)
+      expect(effect).not_to be_valid
+    end
+
+    it 'raises an error if no key is specified' do
+      effect = described_class.new(effect_config.except(:reducer_key))
+      expect do
+        effect.perform(reduction.workflow_id, reduction.subject_id)
+      end.to raise_error(Effects::ExternalWithBasicAuth::InvalidConfiguration)
+    end
+
+    it 'raises an error if no username is specified' do
+      effect = described_class.new(effect_config.except(:username))
+      expect do
+        effect.perform(reduction.workflow_id, reduction.subject_id)
+      end.to raise_error(Effects::ExternalWithBasicAuth::InvalidConfiguration)
+    end
+
+    it 'raises an error if no password is specified' do
+      effect = described_class.new(effect_config.except(:password))
+      expect do
+        effect.perform(reduction.workflow_id, reduction.subject_id)
+      end.to raise_error(Effects::ExternalWithBasicAuth::InvalidConfiguration)
+    end
+  end
+end
