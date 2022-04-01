64eaaedd60dcc2a2d544bf5c9d4f56dc74b44bdf
Campbell Allen <campbell.allen@gmail.com>
2022-04-01 15:13:34 +0100

    uglifier without mangle for asset compilation
    fix the graphiql asset compilation issue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = Uglifier.new(harmony: true, mangle: false)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
