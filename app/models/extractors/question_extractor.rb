fc0437f8215d6f95c41f8ead163ce3a3592a6561
Campbell Allen <campbell.allen@gmail.com>
2022-05-19 12:16:08 +0100

    add reject option for missing task key extracts
    Provide a new QuestionExtractor behaviour to avoid saving extracts with empty payloads when the classification annotation payload doesn't contain the task key
    
    this can be used for branching decision trees that create classification with not all worfklow task annotations and avoids saving junk extract data in the db and triggering reduction processing which is pointless
    
    This is not the default but will be opt in behaviour to preserve the current suite of raise and ignore (save empty extract data) that for extractors in the wild.

--- app/models/extractors/question_extractor.rb
+++ app/models/extractors/question_extractor.rb
@@ -3,35 +3,37 @@ module Extractors
     class MissingAnnotation < StandardError; end
 
     config_field :task_key, default: 'T0'
-    config_field :if_missing, enum: ['error', 'ignore'], default: 'error'
+    config_field :if_missing, enum: %w[error ignore reject], default: 'error'
 
     def extract_data_for(classification)
-      CountingHash.build do |result|
-        fetch_annotations(classification).each do |annotation|
-          value = annotation.fetch("value")
+      extracted_annotations = classification.annotations.fetch(task_key) do |_missing_key|
+        case if_missing
+        when 'error'
+          raise MissingAnnotation, "No annotations for task #{task_key}"
+        when 'ignore'
+          []
+        when 'reject'
+          return Extractor::NoData # return NoData class here to avoid saving an extract in `runs_extractors` class
+        else
+          raise e # unknown error - ensure this is raised for visibility
+        end
+      end
 
-          case value
-          when Array
+      count_extract_answers(extracted_annotations)
+    end
+
+    private
+
+    def count_extract_answers(annotations)
+      CountingHash.build do |result|
+        annotations.each do |annotation|
+          value = annotation.fetch('value')
+          if value.is_a?(Array)
             value.each { |key| result.increment(key) }
           else
             result.increment(value)
           end
         end
-      end
-    end
-
-    private
-
-    def fetch_annotations(classification)
-      classification.annotations.fetch(task_key)
-    rescue KeyError => ex
-      case if_missing
-      when "error"
-        raise MissingAnnotation, "No annotations for task #{task_key}"
-      when "ignore"
-        []
-      else
-        raise ex
       end
     end
   end
