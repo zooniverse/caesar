01413c4fd5487f332f503a152d0444d016849a39
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-10-05 11:23:20 -0500

    update last classifications spec with classification_at check

--- spec/models/filters/filter_by_repeatedness_spec.rb
+++ spec/models/filters/filter_by_repeatedness_spec.rb
@@ -41,12 +41,12 @@ describe Filters::FilterByRepeatedness do
     describe 'set to keep first' do
       it 'keeps the first classification for a given user' do
         extracts = [
-          Extract.new(id: 1, classification_id: 1, user_id: 1, extractor_key: 'a', classification_at:(Time.now - 1.minute)),
-          Extract.new(id: 2, classification_id: 1, user_id: 1, extractor_key: 'b', classification_at:(Time.now - 1.minute)),
-          Extract.new(id: 3, classification_id: 2, user_id: 2, extractor_key: 'a', classification_at:(Time.now - 1.minute)),
-          Extract.new(id: 4, classification_id: 2, user_id: 2, extractor_key: 'b',  classification_at:(Time.now - 1.minute)),
-          Extract.new(id: 5, classification_id: 3, user_id: 1, extractor_key: 'a',  classification_at:(Time.now - 2.minute)),
-          Extract.new(id: 6, classification_id: 3, user_id: 1, extractor_key: 'b', classification_at:(Time.now - 2.minute))
+          Extract.new(id: 1, classification_id: 1, user_id: 1, extractor_key: 'a', classification_at: (Time.now - 1.minute)),
+          Extract.new(id: 2, classification_id: 1, user_id: 1, extractor_key: 'b', classification_at: (Time.now - 1.minute)),
+          Extract.new(id: 3, classification_id: 2, user_id: 2, extractor_key: 'a', classification_at: (Time.now - 1.minute)),
+          Extract.new(id: 4, classification_id: 2, user_id: 2, extractor_key: 'b',  classification_at: (Time.now - 1.minute)),
+          Extract.new(id: 5, classification_id: 3, user_id: 1, extractor_key: 'a',  classification_at: (Time.now - 2.minute)),
+          Extract.new(id: 6, classification_id: 3, user_id: 1, extractor_key: 'b', classification_at: (Time.now - 2.minute))
         ]
         extract_groups = ExtractsForClassification.from(extracts)
         filter = described_class.new(repeated_classifications: "keep_first")
@@ -69,12 +69,12 @@ describe Filters::FilterByRepeatedness do
 
       it 'keeps the last classification for a given user' do
         extracts = [
-          Extract.new(id: 1, classification_id: 1, user_id: 1, extractor_key: "a"),
-          Extract.new(id: 2, classification_id: 1, user_id: 1, extractor_key: "b"),
-          Extract.new(id: 3, classification_id: 2, user_id: 2, extractor_key: "a"),
-          Extract.new(id: 4, classification_id: 2, user_id: 2, extractor_key: "b"),
-          Extract.new(id: 5, classification_id: 3, user_id: 1, extractor_key: "a"),
-          Extract.new(id: 6, classification_id: 3, user_id: 1, extractor_key: "b")
+          Extract.new(id: 1, classification_id: 1, user_id: 1, extractor_key: 'a', classification_at: (Time.now - 1)),
+          Extract.new(id: 2, classification_id: 1, user_id: 1, extractor_key: 'b', classification_at: (Time.now - 1)),
+          Extract.new(id: 3, classification_id: 2, user_id: 2, extractor_key: 'a', classification_at: (Time.now - 1)),
+          Extract.new(id: 4, classification_id: 2, user_id: 2, extractor_key: 'b', classification_at: (Time.now - 1)),
+          Extract.new(id: 5, classification_id: 3, user_id: 1, extractor_key: 'a', classification_at: Time.now),
+          Extract.new(id: 6, classification_id: 3, user_id: 1, extractor_key: 'b', classification_at: Time.now)
         ]
         extract_groups = ExtractsForClassification.from(extracts)
 
