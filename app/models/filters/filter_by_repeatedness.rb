247c32639416b6f51cb3ba4bfbac66bb74eb1b50
Campbell Allen <campbell.allen@gmail.com>
2022-10-06 10:44:56 +0100

    modify the code to clarify how the repeatedness filtering works
    i've always found it hard to understand this code as is, found myself rewriting while reviewing #1448 so thought i'd submit a PR for it. Perhaps it's useful...?

--- app/models/filters/filter_by_repeatedness.rb
+++ app/models/filters/filter_by_repeatedness.rb
@@ -10,28 +10,40 @@ module Filters
       when "keep_all"
         extract_groups
       when "keep_first"
-        keep_first_classification(extract_groups)
+        keep_first_user_classification(extract_groups)
       when "keep_last"
-        keep_first_classification(extract_groups.reverse).reverse
+        keep_last_user_classification(extract_groups)
       end
     end
 
     private
 
-    def keep_first_classification(extracts)
-      subjects ||= Hash.new
+    def keep_last_user_classification(extract_groups)
+      # reverse the list so we can use the same logic as keep_first
+      only_last_classifications = keep_first_user_classification(extract_groups.reverse)
+      # preserve the sort order of the original extract groups
+      only_last_classifications.reverse
+    end
 
-      extracts.select do |extracts_for_classification|
-        subject_id = extracts_for_classification.subject_id
-        user_id = extracts_for_classification.user_id
+    def keep_first_user_classification(extract_groups)
+      subjects_user_has_classified = Hash.new
 
-        subjects[subject_id] = Set.new unless subjects.has_key? subject_id
-        id_list = subjects[subject_id]
+      extract_groups.select do |extracts_from_single_classification|
+        subject_id = extracts_from_single_classification.subject_id
+        user_id = extracts_from_single_classification.user_id
 
-        next true unless extracts_for_classification.user_id
-        next false if id_list.include?(user_id)
-        id_list << user_id
-        true
+        subjects_user_has_classified[subject_id] ||= Set.new
+        user_ids_that_have_classified_subject = subjects_user_has_classified[subject_id]
+
+        case
+        when extracts_from_single_classification.user_id.nil?
+          true # keep anonymous classifications
+        when user_ids_that_have_classified_subject.include?(user_id)
+          false # skip the extract if we've already got one for this user id
+        else
+          user_ids_that_have_classified_subject << user_id
+          true # keep the extract and record the associated user for next lookup
+        end
       end.to_a
     end
 
