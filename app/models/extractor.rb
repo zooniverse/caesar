6d33e61d4cc9411acd3b0177e104ca1f42176211
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-01-09 10:38:18 -0600

    Extractor creation/editing minimum workflow version fix (#1492)
    * update extractor view template to not display any on minimum workflow version if not configured
    
    * updated form partial to only be able to type in numerics to minimum_workflow_version
    
    * add validation on minimum_workflow_version
    
    * add tests on validation min_workflow_version
    
    * update spec to check for numerics and string of numbers for min_workflow_version
    
    * do not allow  negative minimum workflow version

--- app/models/extractor.rb
+++ app/models/extractor.rb
@@ -1,5 +1,6 @@
 class Extractor < ApplicationRecord
   include Configurable
+  validates :minimum_workflow_version, numericality: { greater_than: 0 }, allow_nil: true
 
   class ExtractionFailed < StandardError; end
 
